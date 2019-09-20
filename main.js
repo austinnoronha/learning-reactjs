d56489478120891fe7e0da1dbef1ed8d4cf737aa
Austin Noronha <austinn@rediff.net>
2019-09-20 17:28:53 +0530

    Leacture 3: Structured App - Header + MainContainer + Footer

--- main.js
+++ main.js
@@ -1,28 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const divStyle  = {
-   background: '#dfdfdf',
-   color: '#000',
-   border: '1px solid red',
-   padding: '5px',
-   borderRadius: '5px',
-   fontFamily: 'Helvetica',
-   lineHeight: '20px;'
-};
+// Import Components
+import App from './components/App'
 
-function MyInfo() {
-  return (
-    <div style={divStyle}>
-      <h1>Bob Ziroll</h1>
-      <p>This is a paragraph about me...</p>
-      <ul>
-        <li>Thailand</li>
-        <li>Japan</li>
-        <li>Nordic Countries</li>
-      </ul>
-    </div>
-  )
-}
-
-ReactDOM.render(<MyInfo />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
